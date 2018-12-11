a6e619e3a02dbc41eca3949f5abd6480448b412f
abd11a <abdullah.mohammed@gmail.com>
2018-12-11 18:41:35 -0400

    Add files via upload

---
+++ pyops.py
@@ -0,0 +1,18 @@
+# Custom operators
+# 25 nov 2018
+# AR, AQ, Abdullah
+
+def star_op (firstop, secondop):
+	return ( firstop * 2) + secondop
+
+def squiggle_op (firstop,secondop):
+	return firstop**3 + secondop**2
+
+def multidot (op1,op2):
+	return	(op1*4)//op1	
+
+print ("8 starop 2 == ", star_op (8,2)) # == 18
+print ("3 squiggle 2 == ", squiggle_op (3,2)) # == 31
+print ("2 multidot 2 == ", multidot(2,2)) # == 9
+
+print("\n")
